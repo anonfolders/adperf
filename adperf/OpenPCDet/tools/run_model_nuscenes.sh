1e2ebcfc42cfb161535f479ac8dc9b40b06a0dc6
anon <anon>
2025-03-24 14:23:51 -0400

    add git ignored files

---
+++ adperf/OpenPCDet/tools/run_model_nuscenes.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/bash
+
+# backup LIDAR_TOP
+orig_backup=../data/nuscenes/v1.0-mini/samples/LIDAR_TOP_ORIG
+target=../data/nuscenes/v1.0-mini/samples/LIDAR_TOP
+mv $target $orig_backup
+
+# target the generated PCD for testing
+data=../data/adperf/corruptions/pcds/noise_left_1
+
+# choose model
+# pointpillar
+# model_config=cfgs/nuscenes_models/cbgs_pp_multihead.yaml
+# model=../checkpoints/pp_multihead_nds5823_updated.pth
+
+# centerpoint
+model_config=cfgs/nuscenes_models/cbgs_dyn_pp_centerpoint.yaml
+model=../checkpoints/cbgs_pp_centerpoint_nds6070.pth
+
+# move testing data in place for execution
+cp -r $data $target
+# execute model
+for i in {1..1}
+do
+    python test_adperf.py --choose_dataset nuscenes --cfg_file $model_config --batch_size 1 --workers 1 --ckpt $model
+done
+rm -r $target
+
+# restore backup data
+mv $orig_backup $target
+
